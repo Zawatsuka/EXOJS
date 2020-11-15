6143ffddf02bc89d93b592ad463724944b315a4b
Zawatsuka <thomaslucas2904@gmail.com>
2020-11-15 18:03:18 +0100

    js

---
+++ P2/EXO1/assets/js/exo1.js
@@ -0,0 +1,6 @@
+let number = prompt('Votre chiffre ?');
+if (number > 21){
+    alert (number + ' ' + 'c\'est okay');
+} else {
+    alert ('non');
+}
---
+++ P2/EXO2/assets/js/exo2.js
@@ -0,0 +1,7 @@
+let age = prompt("Votre age ?");
+
+if (age < 18){
+    alert ("vous etes mineur")
+} else {
+    alert ("vous etes majeur")
+}
